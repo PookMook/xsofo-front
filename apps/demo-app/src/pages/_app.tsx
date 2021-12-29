1bdabf641ee902fe2ab3c7daac32ecf935ea1ca4
Monty Schmidt <monty@wuntusk.com>
2021-12-29 08:50:09 -0500

    forgot to remove react query

--- apps/demo-app/src/pages/_app.tsx
+++ apps/demo-app/src/pages/_app.tsx
@@ -1,26 +1,11 @@
 import React from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { globalStyles } from '@xsofo/design-system';
 import type { AppProps } from 'next/app';
 
 const App = function App({ Component, pageProps }: AppProps) {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false,
-        refetchOnReconnect: false,
-        retry: false,
-        staleTime: 5 * 60 * 1000,
-      },
-    },
-  });
   globalStyles();
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
-  );
+  return <Component {...pageProps} />;
 };
 
 export default App;
